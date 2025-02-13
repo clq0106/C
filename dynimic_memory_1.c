1a083a08c4e2d848b03e9264c8dc5888aed0fdb1
clq0106 <158284992+clq0106@users.noreply.github.com>
2025-02-13 02:43:13 +0100

    Add files via upload

---
+++ dynimic_memory_1.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	int *p;
+	char *pp;
+	float *ppp;
+
+	p = 0;
+	pp = 0;
+	ppp = 0;
+	
+	p = malloc(32);
+	pp = malloc(32);
+	ppp = malloc(32);
+
+	scanf("%d", &p);
+	scanf("%s", pp);
+	scanf("%f", &ppp);
+
+	printf("%d, %s, %f, %p, %p, %p\n", p, pp, ppp, &p, &pp, &ppp);
+
+	free(p);
+	free(pp);
+	free(ppp);
+
+
+	return 0;
+}
+	
