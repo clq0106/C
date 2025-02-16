a2c9588b4e22e8edb7878f1ae0d09429bfba6c16
clq0106 <158284992+clq0106@users.noreply.github.com>
2025-02-16 10:25:52 -0800

    Add files via upload

---
+++ xor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define KEY 'X';
+
+int main() {
+	char key, ciphertext[32], newciphertext;
+
+	ciphertext = 0;
+	key = KEY;
+	printf("Geben Sie einen text ein welchen SIe übersetzen wollen\n");
+	scanf("%s", &ciphertext);
+	free(ciphertext);
+
+	for(i != NULL; 
+
+
+	return 0;
+}
