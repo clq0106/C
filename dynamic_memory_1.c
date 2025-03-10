e0001f3cfd853a282b8eb45ea688018a390c794d
clq0106 <158284992+clq0106@users.noreply.github.com>
2025-03-10 08:28:01 -0700

    Update dynamic_memory_1.c

--- dynamic_memory_1.c
+++ dynamic_memory_1.c
@@ -1,15 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int test_function() {
+	variable = 0;
+
+	variable = malloc(32);
+	
+	prtinf("Dies ist eine seperate fundtion, geben sie bitte eine int ein: \t");
+	scanf("%d", variable);
+
+	free(variable);
+	return variable;
+}
 int main() {
 	int *p;
 	char *pp;
 	float *ppp;
+	int *pppp;
 
 	p = 0;
 	pp = 0;
 	ppp = 0;
-	
+
 	p = malloc(32);
 	pp = malloc(32);
 	ppp = malloc(64);
@@ -18,7 +30,8 @@ int main() {
 	scanf("%s", pp);
 	scanf("%f", &ppp);
 
-	printf("%d, %s, %f, %p, %p, %p\n", p, pp, ppp, &p, &pp, &ppp);
+	*pppp = test_function();
+	printf("%d, %s, %f, %d, %p, %p, %p, %p\n", p, pp, ppp, pppp, &p, &pp, &ppp, &pppp);
 
 	free(p);
 	free(pp);
