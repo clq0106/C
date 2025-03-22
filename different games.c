e4950a4ee1f5d3c405ed3c791cc512414ffc4156
clq0106 <158284992+clq0106@users.noreply.github.com>
2025-03-22 17:49:35 +0100

    Update different games.c

--- different games.c
+++ different games.c
@@ -8,28 +8,23 @@ int Zahlenspiel() {
 	printf("Dieses Zahlen raten Spiel hat als Untergrenze 0, geben Sie eine Eingabe fuer das obere Ende: \n");
 	scanf("%d", &N);
 	int value = rand() % (N + 1);
+	*tmp=malloc(sizeof(int));
 	do {
 		printf("Erraten Sie die Zahl: \t");
 		scanf("%d", &eingabe_zahlenspiel);
 		trys = trys+1;
+		*tmp = &trys;
 		if(eingabe_zahlenspiel < value) {
-			tmp=malloc(32);
-			*tmp=&trys;
-			tmp+1;
 			printf("\nDie Zahl ist zu klein\t\t\ttry:%d\n", *ptr);
-			free(tmp);
 		}
 		if(eingabe_zahlenspiel > value) {
-			tmp=malloc(32);
-			*tmp=&trys;
-			tmp+1;
 			printf("\nDie Zahl ist zu gross\t\t\ttry:%d\n",*ptr;
-			free(tmp);
 		}
 		if(eingabe_zahlenspiel == value) {
 			printf("\nSie haben die Zahl gefunden\n");
 		}
 	} while (eingabe_zahlenspiel != value);
+	free(tmp);
 	return trys;	
 }
 int main(int argc, char *argv[]) {
