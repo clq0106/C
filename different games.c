a278fe589860ea102ece4b075c39783ba22a152b
clq0106 <158284992+clq0106@users.noreply.github.com>
2026-04-20 14:28:38 +0200

    Update different games.c

--- different games.c
+++ different games.c
@@ -3,22 +3,22 @@
 #include <math.h>
 
 int Zahlenspiel() {
-	int N, eingabe_zahlenspiel, *tmp;
+	int N, eingabe_zahlenspiel, *tmp, *pA;
 	int trys = 0;
 	printf("Dieses Zahlen raten Spiel hat als Untergrenze 0, geben Sie eine Eingabe fuer das obere Ende: \n");
 	scanf("%d", &N);
 	int value = rand() % (N + 1);
-	*tmp=malloc(sizeof(int));
+//	*tmp=malloc(sizeof(int));
 	do {
 		printf("Erraten Sie die Zahl: \t");
 		scanf("%d", &eingabe_zahlenspiel);
-		trys = trys+1;
-		*tmp = &trys;
+//		trys = trys+1;
+//		*tmp = &trys;
 		if(eingabe_zahlenspiel < value) {
-			printf("\nDie Zahl ist zu klein\t\t\ttry:%d\n", *ptr);
+			printf("\nDie Zahl ist zu klein\t\t\ttry:%d\n", *pA);
 		}
 		if(eingabe_zahlenspiel > value) {
-			printf("\nDie Zahl ist zu gross\t\t\ttry:%d\n",*ptr;
+			printf("\nDie Zahl ist zu gross\t\t\ttry:%d\n",*pA);
 		}
 		if(eingabe_zahlenspiel == value) {
 			printf("\nSie haben die Zahl gefunden\n");
@@ -33,4 +33,3 @@ int main(int argc, char *argv[]) {
 	printf("Die Anzahl von Versuchen die Sie gebraucht haben: %d\n",trys);
 	return 0;
 }
-	
